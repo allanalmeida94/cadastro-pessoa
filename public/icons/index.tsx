3c0c640e42504044a7c17049a01fe92b8f56304c
Allan Almeida <allansoares.almeida@gmail.com>
2024-09-18 09:55:21 -0300

    chore: adiciona os ícones trashIcon e editIcon

--- public/icons/index.tsx
+++ public/icons/index.tsx
@@ -8,3 +8,26 @@ export const googleIcon = () => (
       <path d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z" />
   </svg>
 )
+
+export const trashIcon = (height: string = '1.5em', width: string = '1.5em') => (
+  <svg
+    viewBox="0 0 24 24"
+    fill="none"
+    height={height}
+    width={width}
+     >
+        <path d="M4 6H20M16 6L15.7294 5.18807C15.4671 4.40125 15.3359 4.00784 15.0927 3.71698C14.8779 3.46013 14.6021 3.26132 14.2905 3.13878C13.9376 3 13.523 3 12.6936 3H11.3064C10.477 3 10.0624 3 9.70951 3.13878C9.39792 3.26132 9.12208 3.46013 8.90729 3.71698C8.66405 4.00784 8.53292 4.40125 8.27064 5.18807L8 6M18 6V16.2C18 17.8802 18 18.7202 17.673 19.362C17.3854 19.9265 16.9265 20.3854 16.362 20.673C15.7202 21 14.8802 21 13.2 21H10.8C9.11984 21 8.27976 21 7.63803 20.673C7.07354 20.3854 6.6146 19.9265 6.32698 19.362C6 18.7202 6 17.8802 6 16.2V6M14 10V17M10 10V17" stroke='#ff0000' strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+
+  </svg>
+)
+
+export const editIcon = (height: string = '1.5em', width: string = '1.5em') => (
+  <svg
+    viewBox="0 0 24 24"
+    fill="none"
+    height={height}
+    width={width}
+      >
+        <path d="M20,16v4a2,2,0,0,1-2,2H4a2,2,0,0,1-2-2V6A2,2,0,0,1,4,4H8" fill="none" stroke='#03cd00' strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"></path> <polygon fill="none" points="12.5 15.8 22 6.2 17.8 2 8.3 11.5 8 16 12.5 15.8" stroke='#03cd00' strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"></polygon>
+  </svg>
+)
