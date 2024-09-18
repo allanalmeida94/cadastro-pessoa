b8003951945188db739555def649b7b3a7594651
Allan Almeida <allansoares.almeida@gmail.com>
2024-09-18 09:56:50 -0300

    chore: altera o layout do componente Table

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,4 +1,4 @@
-import Button from './Button';
+import { editIcon, trashIcon } from '../../public/icons';
 import { Person } from './types';
 
 interface TableProps {
@@ -9,53 +9,49 @@ export default function Table({mockData}: TableProps) {
 
   function renderHeader() {
     return (
-      <tr className='text-xl h-10'>
-        <th>CPF</th>
-        <th>Nome</th>
-        <th>idade</th>
-        <th>Sexo</th>
-        <th>Ação</th>
+      <tr className='text-xl text-left'>
+        <th className='p-2'>CPF</th>
+        <th className='p-2'>Nome</th>
+        <th className='p-2'>idade</th>
+        <th className='p-2'>Sexo</th>
+        <th className='p-2 text-center'>Ação</th>
       </tr>
     )
   }
 
   function renderData() {
     return mockData.map((person, index) => (
-      <tr key={person.cpf} className={`${index % 2 === 0 ? 'bg-green-100' : ''} text-lg`}>
-        <td className='border-r-2 pl-2 text-center'>{person.cpf}</td>
-        <td className='border-r-2 pl-2'>{person.name}</td>
-        <td className='border-r-2 pl-2'>{person.age}</td>
-        <td className='border-r-2 pl-2'>{person.gender}</td>
-        <td className='pl-2 text-center'>ação</td>
+      <tr key={person.cpf} className={`${index % 2 === 0 ? 'bg-green-100' : ''} text-lg text-left`}>
+        <td className='p-2'>{person.cpf}</td>
+        <td className='p-2'>{person.name}</td>
+        <td className='p-2'>{person.age}</td>
+        <td className='p-2'>{person.gender}</td>
+       {renderActions()}
       </tr>
     ))
   }
 
+  function renderActions() {
+    return (
+       <td className='flex p-2 justify-center items-center'>
+          <button className='bg-white rounded-full p-2 hover:bg-slate-200'>
+            {trashIcon()}
+          </button>
+          <button className='bg-white rounded-full p-2 ml-3 hover:bg-slate-200'>
+            {editIcon()}
+          </button>
+        </td>
+    )
+  }
+
   return (
-    <div className='flex flex-col w-2/3 p-4 bg-white rounded-xl'>
-      <h1 className='text-2xl mb-6 font-semibold'>
-        Sistema para cadastro de pessoas
-      </h1>
-      <hr className='mb-5 border-blue-300 border-2'/>
-      <div className='flex justify-end mb-5'>
-        <Button className='max-w-max px-4 py-2 rounded-lg bg-blue-500 hover:bg-blue-700 text-white'>
-          Novo cadastro
-        </Button>
-      </div>
       <table className='w-full rounded-md overflow-hidden border'>
-        <thead className='bg-blue-400'>
+        <thead className='bg-blue-500'>
           {renderHeader()}
         </thead>
         <tbody className='mt-2'>
           {renderData()}
         </tbody>
       </table>
-      <hr className='mt-4 border-gray-300 border-2'/>
-      <div className='flex justify-end mt-4'>
-        <Button className='px-4 py-2 bg-red-500 hover:bg-red-700 text-white text-xl rounded-lg'>
-          Sair do sistema
-        </Button>
-      </div>
-    </div>
   )
 }
