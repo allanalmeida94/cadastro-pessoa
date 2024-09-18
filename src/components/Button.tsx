cbebdfb3e448b09c43c2ffe4b0f21ace6bd3cc10
Allan Almeida <allansoares.almeida@gmail.com>
2024-09-18 09:56:17 -0300

    chore: altera o layout do componente Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,14 @@
+import { ReactNode } from 'react'
+
 interface ButtonProps {
-  children: string
+  children: ReactNode
   className?: string
+  handleClick: () => void
 }
 
-export default function Button({children, className}: ButtonProps) {
+export default function Button({children, className, handleClick}: ButtonProps) {
   return (
-    <button className={className}>
+    <button className={`px-4 py-2 rounded-md text-white text-xl ${className}`} onClick={handleClick}>
       {children}
     </button>
   )
