42bdbe5e459a730815f0a976b9f86794e2a0d1cc
Allan Almeida <allansoares.almeida@gmail.com>
2024-09-30 16:56:34 -0300

    chore: altera a estrutura do componente Input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,16 +1,29 @@
-interface InputProps {
-  labelName?: string,
-  nameInput: string,
-  typeInput: string,
-  classNameInput?: string
+import { HTMLInputTypeAttribute } from "react"
+import { UseFormRegisterReturn } from "react-hook-form"
+
+type InputProps = {
+  label: string
+  type: HTMLInputTypeAttribute | undefined
+  error?: string
+  register: UseFormRegisterReturn
 }
 
-export default function Input({labelName, nameInput, typeInput, classNameInput}: InputProps) {
+export default function Input({label, type, error, register, ...rest}: InputProps) {
+
   return (
     <div className='flex flex-col py-3'>
-      <label className='mb-1 text-xl font-semibold'>{labelName}</label>
-      <input type={typeInput} name={nameInput} className={`p-2 rounded-md text-lg border border-gray-900
-        focus:outline-none focus:bg-white focus:border focus:border-blue-500 ${classNameInput}`}/>
+      <label className='mb-1 text-xl font-semibold'>{label}</label>
+      <input
+        {...register}
+        {...rest}
+        type={type}
+        className={`p-2 rounded-md text-lg border border-gray-300
+        outline-none focus:border-blue-500`}/>
+        {error &&
+          <span className="text-red-500 text-md mt-1">
+              {error}
+          </span>
+        }
     </div>
   )
 }
