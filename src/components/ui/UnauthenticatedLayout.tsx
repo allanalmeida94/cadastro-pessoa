8bc1994188fd49770a86e0d8df470acac9f0c9c6
Allan Almeida <allansoares.almeida@gmail.com>
2024-09-30 17:36:59 -0300

    chore: adiciona o layout para usuários não autenticados

---
+++ src/components/ui/UnauthenticatedLayout.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import { ReactElement } from "react";
+import { FaPeopleGroup } from "react-icons/fa6";
+import { fredoka } from "../../../public/fonts";
+
+export default function UnauthenticatedLayout({children}: {children: ReactElement}) {
+  return (
+    <>
+      <header className="flex h-20 justify-center items-center border-b shadow-md">
+        <div className="w-full max-w-[1200px]">
+          <Link href="/">
+            <span className="flex items-center text-blue-700">
+              <FaPeopleGroup size={40} />
+               <h1 className={`ml-3 text-3xl font-bold ${fredoka.className}`}>Cadastro Soft</h1>
+            </span>
+          </Link>
+        </div>
+      </header>
+      {children}
+    </>
+  )
+}
