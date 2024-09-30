0871393fdd4a6db7b1c16fa620f13b97ad8cc3a8
Allan Almeida <allansoares.almeida@gmail.com>
2024-09-30 16:56:07 -0300

    chore: altera a estrutura do componente Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,16 @@
-import { ReactNode } from 'react'
-
-interface ButtonProps {
-  children: ReactNode
-  className?: string
-  handleClick: () => void
+type ButtonProps = {
+  text: string,
+  backgroundColor?: string,
+  type?: "submit" | "reset" | "button" | undefined
+  className?: string,
+  handleClick?: () => void,
+  disabled?: boolean
 }
 
-export default function Button({children, className, handleClick}: ButtonProps) {
+export default function Button({text, type, backgroundColor, className, handleClick, ...props}: ButtonProps) {
   return (
-    <button className={`px-4 py-2 rounded-md text-white text-xl ${className}`} onClick={handleClick}>
-      {children}
+    <button  {...props} onClick={handleClick} type={type} className={`px-4 py-2 rounded-md text-xl ${backgroundColor ? backgroundColor : 'bg-slate-500'} ${className}`}>
+      {text}
     </button>
   )
 }
