9f3782631ec3f82d53708b36dd31e88ad9f734b2
Allan Almeida <allansoares.almeida@gmail.com>
2024-09-30 17:51:14 -0300

    chore: adiciona a página usuários

---
+++ src/pages/usuarios.tsx
@@ -0,0 +1,13 @@
+import MainLayout from "@/components/ui/MainLayout";
+
+export default function Usuários() {
+
+  return (
+    <MainLayout>
+      <div className="flex flex-col w-full h-full p-5">
+        <h1 className="text-2xl font-bold">Usuários do Sistema</h1>
+      </div>
+    </MainLayout>
+  )
+
+}
