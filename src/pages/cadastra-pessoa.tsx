d05618adb17a09e02f40b1aa5b7423a37911f990
Allan Almeida <allansoares.almeida@gmail.com>
2024-09-30 17:51:37 -0300

    chore: adiciona a página cadastra pessoa

---
+++ src/pages/cadastra-pessoa.tsx
@@ -0,0 +1,13 @@
+import MainLayout from "@/components/ui/MainLayout";
+
+export default function CadastraPessoa() {
+
+  return (
+    <MainLayout>
+      <div className="flex flex-col w-full h-full p-5">
+        <h1 className="text-2xl font-bold">Cadastrar pessoa</h1>
+      </div>
+    </MainLayout>
+  )
+
+}
