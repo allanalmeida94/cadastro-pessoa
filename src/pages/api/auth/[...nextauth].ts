35c934a09ca21d3c043c8e957945bb3b110d1beb
Allan Almeida <allansoares.almeida@gmail.com>
2024-10-02 01:04:21 -0300

    feat: adiciona o credencial provider do Nextauth para fazer login com e-mail e senha

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,48 @@
+import User from "@/models/User";
+import connect from "@/utils/database/db";
+import bcrypt from 'bcryptjs';
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions = {
+
+  providers: [
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text", placeholder: "email@example.com" },
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials) {
+
+        try {
+          await connect();
+          const user = await User.findOne({email: credentials?.email})
+
+          if (user) {
+            const validPassword = await bcrypt.compare(
+              credentials?.password as string,
+              user.password
+            )
+
+            if (validPassword) {
+              return user;
+            } else {
+              throw new Error("Credenciais inválidas!");
+            }
+
+          }
+        } catch (error) {
+          throw new Error(error as string)
+        }
+      }
+    })
+  ],
+  pages: {
+    error: "/login",
+    signIn: "/login",
+    signOut: "/"
+  }
+}
+export default NextAuth(authOptions)
