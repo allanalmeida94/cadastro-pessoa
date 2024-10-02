0a8afaf66e9479e05b7ae1aaa6472f19aa285e29
Allan Almeida <allansoares.almeida@gmail.com>
2024-10-02 01:01:33 -0300

    fix: altera o nome do arquivo createUserSchema para userSchema

--- src/utils/schemas/userSchema.ts
+++ src/utils/schemas/userSchema.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
 
-export const createUserSchema = z.object({
+export const userSchema = z.object({
   email: z.string().min(1, 'O campo e-mail é obrigatório').email('E-mail inválido!'),
   password: z.string().min(4, 'A senha deve conter pelo menos 4 dígitos')
 })
 
-export type userData = z.infer<typeof createUserSchema>
+export type userData = z.infer<typeof userSchema>
