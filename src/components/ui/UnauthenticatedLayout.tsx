735a52497e3dafabc82bcf83e53c45d85e0ee73f
Allan Almeida <allansoares.almeida@gmail.com>
2024-10-02 01:02:52 -0300

    fix: altera a largura do componente Link

--- src/components/ui/UnauthenticatedLayout.tsx
+++ src/components/ui/UnauthenticatedLayout.tsx
@@ -8,8 +8,8 @@ export default function UnauthenticatedLayout({children}: {children: ReactElemen
     <>
       <header className="flex h-20 justify-center items-center border-b shadow-md">
         <div className="w-full max-w-[1200px]">
-          <Link href="/">
-            <span className="flex items-center text-blue-700">
+          <Link href="/" className="flex w-max">
+            <span className="flex items-center w-max text-blue-700">
               <FaPeopleGroup size={40} />
                <h1 className={`ml-3 text-3xl font-bold ${fredoka.className}`}>Cadastro Soft</h1>
             </span>
