c1eeb138ca1a653d411b000cf5588d8b79608b43
Allan Almeida <allansoares.almeida@gmail.com>
2024-10-02 01:17:58 -0300

    chore: adiciona validação de sessão no componente MainLayout

--- src/components/ui/MainLayout.tsx
+++ src/components/ui/MainLayout.tsx
@@ -1,14 +1,34 @@
+import { signIn, signOut, useSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
 import { ReactElement } from 'react'
 import { FaSignOutAlt } from 'react-icons/fa'
 import { FaPeopleGroup } from 'react-icons/fa6'
 import Navbar from '../Navbar'
 
 export default function MainLayout({children}: {children: ReactElement}) {
+
+  const router = useRouter();
+
+  const {data: session, status} = useSession();
+
+  if (status === "loading") {
+    return null
+  }
+
+  if (!session) {
+    signIn();
+    return null
+  }
+
+  function handleHome() {
+    router.push('/')
+  }
+
   return (
     <div className='flex min-h-screen'>
       <div className='flex flex-col items-center border-r shadow-md'>
         <div className='flex flex-col flex-grow w-[250px]'>
-          <div className='flex flex-col justify-center items-center h-20 bg-blue-900 text-white'>
+          <div onClick={handleHome} className='flex flex-col justify-center items-center h-20 bg-blue-900 text-white hover:cursor-pointer'>
               <FaPeopleGroup size={40}/>
               <span className='text-xl font-semibold'>
                 Cadastro Soft
@@ -18,7 +38,7 @@ export default function MainLayout({children}: {children: ReactElement}) {
             <Navbar />
           </aside>
         </div>
-        <button className='flex justify-center items-center px-4 py-2 mb-10 border-none text-xl font-semibold hover:text-red-700'>
+        <button onClick={() => signOut()} className='flex justify-center items-center px-4 py-2 mb-10 border-none text-xl font-semibold hover:text-red-700'>
           Sair <span className='ml-2'><FaSignOutAlt size={20}/></span>
         </button>
       </div>
