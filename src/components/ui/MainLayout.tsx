b35b55b00e31d3868e07dfa957b0d60223419f3d
Allan Almeida <allansoares.almeida@gmail.com>
2024-10-02 21:36:15 -0300

    chore: altera a validação de sessão e de logout no layout principal

--- src/components/ui/MainLayout.tsx
+++ src/components/ui/MainLayout.tsx
@@ -1,34 +1,30 @@
-import { signIn, signOut, useSession } from 'next-auth/react'
-import { useRouter } from 'next/navigation'
+"use client"
+import { signOut, useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
 import { ReactElement } from 'react'
 import { FaSignOutAlt } from 'react-icons/fa'
 import { FaPeopleGroup } from 'react-icons/fa6'
 import Navbar from '../Navbar'
 
 export default function MainLayout({children}: {children: ReactElement}) {
-
   const router = useRouter();
-
-  const {data: session, status} = useSession();
+  const {data: session, status} = useSession({required: true, onUnauthenticated() {
+    router.push('/login')
+  },});
 
   if (status === "loading") {
     return null
   }
 
   if (!session) {
-    signIn();
-    return null
-  }
-
-  function handleHome() {
-    router.push('/')
+    router.push('/login')
   }
 
   return (
     <div className='flex min-h-screen'>
       <div className='flex flex-col items-center border-r shadow-md'>
         <div className='flex flex-col flex-grow w-[250px]'>
-          <div onClick={handleHome} className='flex flex-col justify-center items-center h-20 bg-blue-900 text-white hover:cursor-pointer'>
+          <div className='flex flex-col justify-center items-center h-20 bg-blue-900 text-white'>
               <FaPeopleGroup size={40}/>
               <span className='text-xl font-semibold'>
                 Cadastro Soft
@@ -38,7 +34,11 @@ export default function MainLayout({children}: {children: ReactElement}) {
             <Navbar />
           </aside>
         </div>
-        <button onClick={() => signOut()} className='flex justify-center items-center px-4 py-2 mb-10 border-none text-xl font-semibold hover:text-red-700'>
+        <button onClick={async () => {
+          const data = await signOut({redirect: false, callbackUrl: "/login"})
+          router.push(data.url)
+          }}
+          className='flex justify-center items-center px-4 py-2 mb-10 border-none text-xl font-semibold hover:text-red-700'>
           Sair <span className='ml-2'><FaSignOutAlt size={20}/></span>
         </button>
       </div>
